89c73429883f0338c81fdc55229a6c0d28834c7e
Yi Wang <yi.wang.2005@gmail.com>
2018-06-18 15:50:24 -0700

    Update

--- src/variable.cc
+++ src/variable.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/contrib/tape/variable.h"
+#include "variable.h"
 
 namespace paddle {
 namespace tape {
