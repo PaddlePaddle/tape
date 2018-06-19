328213f66d1e01937898bbcc7083debc30ed1738
Yang Yang <yangyang62@baidu.com>
2018-06-19 20:35:40 +0000

    fix cpplint

--- src/test_tape.cc
+++ src/test_tape.cc
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "function.h"
 #include "gtest/gtest.h"
+#include "src/function.h"
 
 using paddle::tape::VariableHandle;
 using paddle::tape::Variable;
