88f74495dd529635d021ae86442f8c40d4fbb519
Yang Yang <yangyang62@baidu.com>
2018-06-22 23:23:11 +0000

    pass mnist

--- src/test_tape.cc
+++ src/test_tape.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <fstream>
+
 #include "gtest/gtest.h"
 #include "src/function.h"
 
@@ -19,9 +21,9 @@ using paddle::tape::VariableHandle;
 using paddle::tape::Variable;
 using paddle::tape::Linear;
 using paddle::tape::Convolution2D;
-using paddle::tape::Mean;
 using paddle::tape::SGD;
 using paddle::tape::Fill;
+using paddle::tape::mean;
 using paddle::tape::softmax;
 using paddle::tape::cross_entropy;
 using paddle::tape::reset_global_tape;
@@ -29,43 +31,69 @@ using paddle::tape::get_global_tape;
 using paddle::tape::CreateRecordioFileReader;
 using paddle::tape::ReadNext;
 
-TEST(Tape, TestReader) {
-  VariableHandle data_label(new paddle::tape::Variable("data_label"));
-  VariableHandle reader = CreateRecordioFileReader(
-      "/tape/src/data/mnist.recordio", {32, 1, 28, 28, 32, 1}, {4, 2}, {0, 0});
-  ReadNext(reader, data_label);
-  LOG(INFO) << *data_label;
+bool is_file_exist(const std::string& fileName) {
+  std::ifstream infile(fileName);
+  return infile.good();
 }
 
-TEST(Tape, TestRelu) {
-  std::string initializer = "uniform_random";
-  paddle::framework::AttributeMap attrs;
-  attrs["min"] = -1.0f;
-  attrs["max"] = 1.0f;
-  attrs["dtype"] = paddle::framework::proto::VarType::Type::VarType_Type_FP32;
-  attrs["shape"] = std::vector<int>{10};
-  attrs["seed"] = 123;
-  Fill filler(initializer, attrs);
+TEST(Tape, TestMnist) {
+  std::string filename = "/tmp/mnist.recordio";
+  PADDLE_ENFORCE(
+      is_file_exist(filename),
+      "file doesn't exist; have you run data/create_mnist_recordio.py");
+  VariableHandle reader = CreateRecordioFileReader(
+      "/tmp/mnist.recordio", {32, 1, 28, 28, 32, 1}, {4, 2}, {0, 0});
 
-  VariableHandle input(new Variable("input"));
-  filler(input);
-  auto loss = relu(input);
-  LOG(INFO) << input->Value();
-  LOG(INFO) << loss->Value();
+  Linear linear1(784, 200, "relu");
+  Linear linear2(200, 200, "relu");
+  Linear linear3(200, 10, "relu");
+  SGD sgd(0.001);
+
+  int print_step = 100;
+  float avg_loss = 0.0;
+
+  for (int i = 0; i < 1000000; ++i) {
+    reset_global_tape();
+    auto data_label = ReadNext(reader);
+    auto data = data_label[0];
+    auto label = data_label[1];
+
+    auto predict = softmax(linear3(linear2(linear1(data))));
+    auto loss = mean(cross_entropy(predict, label));
+    if (i % print_step == 0) {
+      avg_loss +=
+          loss->Value().Get<paddle::framework::LoDTensor>().data<float>()[0];
+      LOG(INFO) << avg_loss;
+      avg_loss = 0;
+    }
+
+    get_global_tape().Backward(loss);
+
+    for (auto w : linear1.Params()) {
+      sgd.Update(w);
+    }
+    for (auto w : linear2.Params()) {
+      sgd.Update(w);
+    }
+    for (auto w : linear3.Params()) {
+      sgd.Update(w);
+    }
+  }
 }
 
 TEST(Tape, TestConv) {
   Convolution2D conv1(3, 16, 3, "relu");
   Convolution2D conv2(16, 1, 3, "relu");
-  Mean mean;
 
   SGD sgd(0.001);
 
-  std::string initializer = "fill_constant";
+  std::string initializer = "uniform_random";
   paddle::framework::AttributeMap attrs;
+  attrs["min"] = -1.0f;
+  attrs["max"] = 1.0f;
   attrs["dtype"] = paddle::framework::proto::VarType::Type::VarType_Type_FP32;
+  attrs["seed"] = 123;
   attrs["shape"] = std::vector<int>{32, 3, 8, 8};
-  attrs["value"] = 1.0f;
   Fill filler(initializer, attrs);
 
   for (int i = 0; i < 2; ++i) {
@@ -90,15 +118,16 @@ TEST(Tape, TestConv) {
 TEST(Tape, TestMLP) {
   Linear linear1(3, 3, "relu");
   Linear linear2(3, 3, "relu");
-  Mean mean;
 
   SGD sgd(0.001);
 
-  std::string initializer = "fill_constant";
+  std::string initializer = "uniform_random";
   paddle::framework::AttributeMap attrs;
+  attrs["min"] = -1.0f;
+  attrs["max"] = 1.0f;
   attrs["dtype"] = paddle::framework::proto::VarType::Type::VarType_Type_FP32;
+  attrs["seed"] = 123;
   attrs["shape"] = std::vector<int>{3, 3};
-  attrs["value"] = 1.0f;
   Fill filler(initializer, attrs);
 
   for (int i = 0; i < 2; ++i) {
@@ -121,7 +150,7 @@ TEST(Tape, TestMLP) {
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   std::vector<paddle::platform::Place> places;
   places.emplace_back(paddle::platform::CPUPlace());
   paddle::platform::DeviceContextPool::Init(places);
